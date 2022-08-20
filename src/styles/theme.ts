61c716dc39a7083a4fa5ad3476058dc42b5c4a14
Jaehyeok <milkboy2564@naver.com>
2022-08-20 17:27:06 +0900

    feat: set global style

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#e9ecef",
+  btnColor: "#868e96",
+  checkBoxColor: "#22b8cf",
+  cardColor: "white",
+  boardColor: "#DADFE9",
+};
