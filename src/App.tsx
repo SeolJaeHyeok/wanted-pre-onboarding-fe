9be33ecb1e1f6e80d65f1201911b429429c6297d
Jaehyeok <milkboy2564@naver.com>
2022-08-20 17:30:14 +0900

    feat: set route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import styled from "styled-components"
-
-const Initial = styled.div`
-  font-size: 200px;
-`;
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Home from './pages/Home/Home';
+import ToDo from './pages/ToDo/ToDo';
 
 function App() {
   return (
-    <Initial>Hello</Initial>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/todo' element={<ToDo />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
