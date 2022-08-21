d3519496ac5440d7fdad6e452689862ac70904b5
Jaehyeok <milkboy2564@naver.com>
2022-08-21 21:05:17 +0900

    feat: todo list components

--- src/pages/ToDo/ToDo.tsx
+++ src/pages/ToDo/ToDo.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getToken } from "../../utils/func";
+import ToDoInput from "./components/ToDoInput";
+import ToDoList from "./components/ToDoList";
+import ToDoTemplate from "./components/ToDoTemplate";
 
 const ToDo = () => {
   const navigate = useNavigate();
@@ -12,7 +15,10 @@ const ToDo = () => {
     }
   }, []);
 
-  return <div>ToDo</div>;
+  return <ToDoTemplate>
+    <ToDoInput />
+    <ToDoList />
+  </ToDoTemplate>;
 }
 
 export default ToDo;
---
+++ src/pages/ToDo/components/ToDoInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToDoInput = () => {
+  return <div>ToDo Input</div>
+}
+
+export default ToDoInput;
